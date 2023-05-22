215f79d0cb33d40574c5894d7111ff19eca290d3
dg1223 <alavi1223@hotmail.com>
2023-05-22 02:12:36 -0400

    search functionality

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -1,18 +1,30 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ServiceCard from "./ServiceCard";
 
 const Services = () => {
   const [services, setServices] = useState([]);
   const [asc, setAsc] = useState(true);
+  const searchRef = useRef(null);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     // fetch("https://mongodb-expressjs-car-doctor-server.vercel.app/services")
-    fetch(`http://localhost:5000/services?sort=${asc ? "asc" : "desc"}`)
+    fetch(
+      `http://localhost:5000/services?search=${search}&sort=${
+        asc ? "asc" : "desc"
+      }`
+    )
       .then((res) => res.json())
       .then((data) => setServices(data));
     // when you read a data inside useEffect, you have to add
     // that as a dependency because useEffect is dependent on it
-  }, [asc]);
+  }, [asc, search]);
+
+  const handleSearch = () => {
+    console.log(searchRef.current.value);
+    setSearch(searchRef.current.value);
+  };
+
   return (
     <div className="mt-4">
       <div className="text-center">
@@ -23,6 +35,32 @@ const Services = () => {
           humour, or randomised <br /> words which do not look even slightly
           believable.
         </p>
+        <div className="form-control">
+          <div className="input-group">
+            <input
+              type="text"
+              placeholder="Search…"
+              ref={searchRef}
+              className="input input-bordered"
+            />
+            <button onClick={handleSearch} className="btn btn-square">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
         <button className="btn btn-primary" onClick={() => setAsc(!asc)}>
           {asc ? "Price High to Low" : "Price Low to High"}
         </button>
